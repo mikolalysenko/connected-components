4a92134860d8df86c325104efa6f097d9ca54887
Mikola Lysenko <mikolalysenko@gmail.com>
2014-04-21 01:16:50 -0500

    update testcase

--- test/test.js
+++ test/test.js
@@ -5,7 +5,17 @@ var findCC = require("../cc.js")
 
 tape("connected-components", function(t) {
 
-  
+  var adjList = [
+    [],
+    [2],
+    [1],
+    [4, 5],
+    [3],
+    [3]
+  ]
+
+  t.same(findCC(adjList), 
+    [ [0], [1, 2], [3, 4, 5] ])
 
   t.end()
 })
